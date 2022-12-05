cdc56a0ca5450da1b78fcc0512f71b7034feccc5
zeworks <josenogueira95@hotmail.com>
2022-12-05 02:01:28 +0000

    add DropdownUser component

--- packages/web/src/layouts/Shell/components/Header/Header.tsx
+++ packages/web/src/layouts/Shell/components/Header/Header.tsx
@@ -1,9 +1,7 @@
-import { Avatar } from "@/components/Avatar";
-import { useSession } from "@/context/Session";
 import { theme } from "@oriun/ui"
 import { Icon } from "@oriun/ui/lib/components/Icon";
-import { Text } from "@oriun/ui/lib/components/Typography";
 import { Dropdown } from "@oriun/ui/src/components/Dropdown";
+import { DropdownUser } from "./components/DropdownUser";
 
 const Root = theme.styled("div", {
   height: "4rem",
@@ -20,12 +18,6 @@ const Root = theme.styled("div", {
 });
 
 export function Header() {
-  const { session, closeAuthentication } = useSession();
-
-  function logout() {
-    closeAuthentication()
-  }
-
   return (
     <Root>
       <div className="flex-fill d-flex justify-content-between">
@@ -45,23 +37,7 @@ export function Header() {
             </Dropdown.Root>
           </div>
           <div className="user">
-            <Dropdown.Root>
-              <Dropdown.Trigger>
-                <div className="d-flex align-items-center">
-                  <div className="d-flex flex-column align-items-end me-2">
-                    <Text>{session?.data?.me?.username}</Text>
-                    <Text variant="small">{session?.data?.me?.role?.name}</Text>
-                  </div>
-                  <Avatar src={session?.data?.me?.profile.picture} />
-                </div>
-              </Dropdown.Trigger>
-              <Dropdown.Content position="right">
-                <Dropdown.Item>Profile</Dropdown.Item>
-                <Dropdown.Separator />
-                <Dropdown.Item>Settings</Dropdown.Item>
-                <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
-              </Dropdown.Content>
-            </Dropdown.Root>
+            <DropdownUser />
           </div>
         </div>
       </div>
---
+++ packages/web/src/layouts/Shell/components/Header/components/DropdownUser.tsx
@@ -0,0 +1,32 @@
+import { Avatar } from "@/components/Avatar";
+import { useSession } from "@/context/Session";
+import { Text } from "@oriun/ui/lib/components/Typography";
+import { Dropdown } from "@oriun/ui/src/components/Dropdown";
+
+export function DropdownUser() {
+  const { session, closeAuthentication } = useSession();
+
+  function logout() {
+    closeAuthentication()
+  }
+
+  return (
+    <Dropdown.Root>
+      <Dropdown.Trigger>
+        <div className="d-flex align-items-center">
+          <div className="d-flex flex-column align-items-end me-2">
+            <Text>{session?.data?.me?.username}</Text>
+            <Text variant="small">{session?.data?.me?.role?.name}</Text>
+          </div>
+          <Avatar src={session?.data?.me?.profile.picture} />
+        </div>
+      </Dropdown.Trigger>
+      <Dropdown.Content position="right">
+        <Dropdown.Item>Profile</Dropdown.Item>
+        <Dropdown.Separator />
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+      </Dropdown.Content>
+    </Dropdown.Root>
+  )
+}
