e08502b7652eab0fdae1eb19ed9bb7cc9dc9d1b2
zeworks <josenogueira95@hotmail.com>
2023-06-10 02:56:15 +0100

    remove autoComplete from input

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -1,27 +1,45 @@
-import React, { ChangeEvent, Ref, useCallback } from 'react'
-import { styled } from '@stitches/react'
-import { InputCSS } from '../../styles/input'
+import React, { ChangeEvent, Ref, useCallback } from "react"
+import { styled } from "@stitches/react"
+import { InputCSS } from "../../styles/input"
 
-export const StyledInput = styled('input', InputCSS);
+export const StyledInput = styled("input", InputCSS)
 
-type InputHTMLAttributes = Pick<React.InputHTMLAttributes<any>, "disabled" | "required" | "value" | "id" | "className" | "onBlur" | "onFocus" | "placeholder">
+type InputHTMLAttributes = Pick<
+	React.InputHTMLAttributes<any>,
+	| "disabled"
+	| "required"
+	| "value"
+	| "id"
+	| "className"
+	| "onBlur"
+	| "onFocus"
+	| "placeholder"
+>
 
 export interface InputProps extends InputHTMLAttributes {
-  innerRef?: Ref<any>;
-  name: string;
-  type: string;
-  onChange?: (value: string, name: string, valid: boolean) => void;
+	innerRef?: Ref<any>
+	name: string
+	type: string
+	onChange?: (value: string, name: string, valid: boolean) => void
 }
 
 export function Input({ name, onChange, innerRef, ...props }: InputProps) {
+	const onInputChange = useCallback(
+		(e: ChangeEvent<HTMLInputElement>) => {
+			const value = e.target.value
+			const valid = e.target.validity.valid || false
+			onChange?.(value, name, valid)
+		},
+		[name, onChange]
+	)
 
-  const onInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    const valid = e.target.validity.valid || false;
-    onChange?.(value, name, valid)
-  }, [name, onChange])
-
-  return (
-    <StyledInput name={name} onChange={onInputChange} ref={innerRef} {...props} />
-  )
+	return (
+		<StyledInput
+			autoComplete="off"
+			name={name}
+			onChange={onInputChange}
+			ref={innerRef}
+			{...props}
+		/>
+	)
 }
